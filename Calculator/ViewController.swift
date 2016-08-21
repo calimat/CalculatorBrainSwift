d595dd4e5f392933b74ef89a3a264b04f52fc732
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2016-08-21 12:52:40 -0500

    Fixes for floating point number on calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var display: UILabel!
-   
-     var userIsInTheMiddleOfTyping = false;
+    
+    var userIsInTheMiddleOfTyping = false;
     
     @IBAction func touchDigit(sender: UIButton) {
         
@@ -20,15 +20,46 @@ class ViewController: UIViewController {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTyping {
             let textCurrentlyInDisplay = display.text!
-            display!.text = textCurrentlyInDisplay + digit
+            
+            //this is a more elegant solution
+            if textCurrentlyInDisplay.rangeOfString(".") != nil && digit == "." {
+                return
+            } else
+            {
+                display!.text = textCurrentlyInDisplay + digit
+            }
+            
+            //This is first try
+            //Exactly the same as above but in one line this
+            /*
+            if textCurrentlyInDisplay.rangeOfString(".") == nil {
+                display!.text = textCurrentlyInDisplay + digit
+            } else
+            {
+                if digit != "." {
+                    display!.text = textCurrentlyInDisplay + digit
+                    
+                }
+                
+                */
+            //}
+            
         }
         else{
-            display.text = digit
+            
+            if digit == "."
+            {
+                display.text = "0."
+            }
+            else {
+                 display.text = digit
+            }
+           
         }
         userIsInTheMiddleOfTyping = true
     }
     
-  
+    
     var displayValue: Double {
         get {
             return Double(display.text!)!
@@ -37,15 +68,15 @@ class ViewController: UIViewController {
             display.text = String(newValue)
         }
     }
-
+    
     private var brain = CalculatorBrain()
     
     @IBAction private func performOperation(sender: UIButton) {
-       
+        
         
         if userIsInTheMiddleOfTyping {
             brain.setOperand(displayValue)
-             userIsInTheMiddleOfTyping = false
+            userIsInTheMiddleOfTyping = false
         }
         
         if let mathematicalSymbol = sender.currentTitle {
@@ -55,6 +86,6 @@ class ViewController: UIViewController {
         displayValue = brain.result
         
     }
-
+    
 }
 
